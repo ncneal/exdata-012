72f729bf702aec7fa3c57010079a08411d1ac283
Nick Neal <ncneal@gmail.com>
2015-03-08 16:02:17 -0700

    initial commit

--- project1/plot1.R
+++ project1/plot1.R
@@ -1,1 +1,3 @@
+png(file = "./project1/plot1.png")
 hist(householdPowerConsumption$Global_active_power, col="red", xlab="Global Active Power (Kilowatts)", main="Global Active Power", ylim=c(0,1200))
+dev.off()
